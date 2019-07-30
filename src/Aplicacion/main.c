4ae6ee244334245c93ae1f674f8f8f979ae74c8d
Fernando <fcoda.96@gmail.com>
2019-07-30 11:48:08 -0300

    Add LPCOpen's USB library and virtual serial port
    Taken as is, just testing stuff...

--- src/Aplicacion/main.c
+++ src/Aplicacion/main.c
@@ -8,20 +8,14 @@
 #include "../Drivers/i2c.h"
 #include "../Drivers/delay.h"
 #include "../Drivers/ssd1306.h"
+#include "../vcom/cdc_vcom.h"
 #include "Aplicacion.h"
-//#include "cdc_vcom.h"
+
 
 extern void kit_init(void);
 
 // todo: deshacerme de las structs globales (hacerlas static) y reemplazar por funciones primitivas
 extern uart_t uart0;
-
-
-//static void dummy(void) {}
-
-
-const uint32_t OscRateIn = 12000000;
-const uint32_t RTCOscRateIn = 32768;
 
 static u8g2_t u8g2;
 
@@ -32,22 +26,8 @@ static u8g2_t u8g2;
 int main(void)
 {
 	kit_init();
-//	setupIrcClocking();
-
-#if 0
-	configurePin(0,   0,  PINMODE_NONE, PINSEL_FUNC2),	/* TXD3 */
-	configurePin(0,   1,  PINMODE_NONE, PINSEL_FUNC2),	/* RXD3 */
-	configurePin(0,   4,  PINMODE_NONE, PINSEL_FUNC2),	/* CAN-RD2 */
-	configurePin(0,   5,  PINMODE_NONE, PINSEL_FUNC2),	/* CAN-TD2 */
-	configurePin(0,  22,  PINMODE_NONE, PINSEL_GPIO ),	/* Led 0 */
-	configurePin(0,  23,  PINMODE_NONE, PINSEL_FUNC1),	/* ADC 0 */
-	configurePin(0,  26,  PINMODE_NONE, PINSEL_FUNC2),	/* DAC */
-	configurePin(1,  27,  PINMODE_NONE, PINSEL_FUNC1);
-	setupClocking();
-	SystemCoreClockUpdate();
-	usbd_init();
-#endif
 	ssd1306_init(&u8g2);
+
 //	u8x8_SendF(&u8g2.u8x8, "c", 0xae); // turn off OLED
 
 	ledBlink();
@@ -67,7 +47,7 @@ int main(void)
 
 		sm_ssd1306(&u8g2);
 
-//		sm_usbd();
+		vcom_usbd();
 
 		__asm volatile ("wfi"); /* sleep until next interruption */
 	}
--- src/Primitivas/PR_kitInit.c
+++ src/Primitivas/PR_kitInit.c
@@ -1,3 +1,4 @@
+#include "../Drivers/chip.h"
 #include "../Drivers/regsLPC1769.h"
 #include "../Drivers/KitInfo2_BaseBoard.h"
 #include "../Drivers/Timer.h"
@@ -5,35 +6,38 @@
 #include "../Drivers/LCD.h"
 #include "../Drivers/UART.h"
 #include "../Drivers/ADC.h"
-#include "../Drivers/chip.h"
+#include "../Drivers/digital_outputs.h"
+#include "../vcom/cdc_vcom.h"
 
-void D_IN_init		( void );
+
 void ExtInt_init	( void );
 void SysTick_init	( uint32_t us );
 void RIT_init		( uint32_t ms );
 
 void kit_init ( void )
 {
-	PLL_init();
+//	PLL_init();
 	power_init();
+//	setupIrcClocking();
+	setupClocking();
 	SystemCoreClockUpdate();
+
 	PIN_init();
-	SysTick_init(2500); // 2.5ms
+	D_OUT_init();
+
+	usbd_init();
+	UART0_init(U0_INIT_STATUS);
+	ADC_init();
+	RTC_init();
+	ExtInt_init();
+
 	TIMER0_init(1000);  // 1ms
 	RIT_init(200);		// 200us
-
-//	delay_ms(500); // temporal, para evitar problemas
-
-	RTC_init();
-	D_IN_init();
-	ExtInt_init();
+	SysTick_init(2500); // 2.5ms
 
 #ifdef DEBUG
 	LCD_init(LCD_INTERNAL_RESET_DISABLED);
 #else
 	LCD_init(LCD_INTERNAL_RESET_ENABLED);
 #endif
-
-	UART0_init(U0_INIT_STATUS);
-	ADC_init();
 }
