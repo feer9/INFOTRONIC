78061b9ac57faeed5547675d65a66e38c3b6868a
feer9 <fcoda.96@gmail.com>
2019-08-09 11:42:31 -0300

    Change to static adc struct
    Replaced global access to the struc
    with functions which set and get values

--- src/Drivers/ADC.c
+++ src/Drivers/ADC.c
@@ -1,33 +1,53 @@
 #include "ADC.h"
 
-ADC_t adc = {
+
+// Control Register setting bits
+#define		CR_SEL		0
+#define		CR_CLKDIV	8
+#define		CR_BURST	16
+#define		CR_PDN		21
+#define		CR_START	24
+#define		CR_EDGE		27
+
+struct ADC {
+	__RW uint32_t AD5_val;
+	__RW bool change;
+	bool status;
+} ;
+
+static struct ADC adc = {
+		.status = false,
 		.AD5_val = 0,
-		.change = false,
-		.timerId = -1
+		.change = true,
 };
+
+inline bool     ADC_hasChanged (void) { return adc.change;  }
+inline void     ADC_clearChange(void) { adc.change = false; }
+inline uint32_t ADC_getVal     (void) { return adc.AD5_val;     }
+inline bool     ADC_isOn       (void) { return adc.status;  }
 
 void ADC_init()
 {
 	PCONP |= PCONP_ADC;
 	// PDN = 1 => The A/D Converter is operational
-	ADC->ADCR = _BIT(CR_PDN);
+	LPC_ADC->ADCR = _BIT(CR_PDN);
 
-	// seteo la fuente de clock, core clock / 8 = 12,5MHz
+	// seteo la fuente de clock, core clock / 8 = 96/8 = 12MHz
 	setPCLKDiv(PCLKSEL_ADC, PCLKDIV_8);
 
 	// seteo el divisor de clock (tiene q quedar <= 13MHZ)
 	// CLKDIV = 1 + este valor
-	ADC->ADCR |= (99UL << CR_CLKDIV);
+	LPC_ADC->ADCR = (0UL << CR_CLKDIV); // (12MHz / 0+1) = 12MHz (< 13MHz)
 
 	set_dir(ADC5, ENTRADA);
 	setPINSEL(ADC5, PINSEL_FUNC3);
 	setPINMODE(ADC5, PINMODE_NONE);
 
 	// seteo AD0.5 como fuente de entrada analogica
-	ADC->ADCR |= (1UL << 5);
+	LPC_ADC->ADCR |= (1UL << 5);
 
 	// Habilito interrupcion para AD0.5
-	ADC->ADINTEN = (1UL << 5);
+	LPC_ADC->ADINTEN = (1UL << 5);
 
 	// Lo dejo apagado porque no lo estoy usando
 	ADC_stop();
@@ -36,25 +56,27 @@ void ADC_init()
 void ADC_stop()
 {
 	setPINSEL(ADC5, PINSEL_GPIO);
-	ADC->ADCR _RESET_BIT(CR_BURST);	// Conversions are software controlled
-	ADC->ADCR _RESET_BIT(CR_PDN);	// The A/D converter is in power-down mode
+	LPC_ADC->ADCR _RESET_BIT(CR_BURST);	// Conversions are software controlled
+	LPC_ADC->ADCR _RESET_BIT(CR_PDN);	// The A/D converter is in power-down mode
 	PCONP &= ~PCONP_ADC;
 	ICER0 = NVIC_ADC;
+	adc.status = false;
 }
 
 void ADC_start()
 {
 	PCONP |= PCONP_ADC;
-	ADC->ADCR _SET_BIT(CR_PDN);		// The A/D converter is operational
-	ADC->ADCR _SET_BIT(CR_BURST);	// The A/D converter does repeated conversions at up to 200 kHz
+	LPC_ADC->ADCR _SET_BIT(CR_PDN);		// The A/D converter is operational
+	LPC_ADC->ADCR _SET_BIT(CR_BURST);	// The A/D converter does repeated conversions at up to 200 kHz
 	setPINSEL(ADC5, PINSEL_FUNC3);
 	ISER0 = NVIC_ADC;
+	adc.status = true;
 }
 
 void ADC_IRQHandler()
 {
 	static uint32_t prev = 0;
-	uint32_t dr5 = ADC->ADDR5;
+	uint32_t dr5 = LPC_ADC->ADDR5;
 
 	if(dr5 >> 31) // DONE
 	{
--- src/Drivers/ADC.h
+++ src/Drivers/ADC.h
@@ -1,28 +1,18 @@
 #ifndef DRIVERS_ADC_H_
 #define DRIVERS_ADC_H_
 
-#include <chip.h>
-#include "../Aplicacion/Aplicacion.h"
-#include "regsLPC1769.h"
+#include "chip.h"
 #include "KitInfo2_BaseBoard.h"
+#include "../Aplicacion/Aplicacion.h"
 
-
-// Control Register setting bits
-#define		CR_SEL		0
-#define		CR_CLKDIV	8
-#define		CR_BURST	16
-#define		CR_PDN		21
-#define		CR_START	24
-#define		CR_EDGE		27
-
-typedef struct {
-	uint32_t AD5_val;
-	__RW bool change;
-	__RW int8_t timerId;
-} ADC_t;
 
 void ADC_init(void);
 void ADC_start();
 void ADC_stop();
+
+bool     ADC_hasChanged (void);
+void     ADC_clearChange(void);
+uint32_t ADC_getVal     (void);
+bool     ADC_isOn       (void);
 
 #endif /* DRIVERS_ADC_H_ */
